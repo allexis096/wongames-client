0a1e075e86d9a399329c8344a8d53de3715587aa
allexis096 <allexissoares@gmail.com>
2021-05-02 19:28:41 -0300

    :sparkles: feat: add Logo component with tests and stories

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Logo, { LogoProps } from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  argTypes: {
+    color: {
+      control: {
+        type: 'radio',
+        options: ['white', 'black'],
+      },
+    },
+    size: {
+      control: {
+        type: 'radio',
+        options: ['normal', 'large'],
+      },
+    },
+  },
+} as Meta
+
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />
