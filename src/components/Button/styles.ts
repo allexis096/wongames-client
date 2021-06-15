9bfb107a2adec56dd26fcf7dd4f954c6a68fa2f9
allexis096 <allexissoares@gmail.com>
2021-06-15 20:21:02 -0300

    :sparkles: chore: upgrade Button component to show minimal

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,10 @@
+import { darken } from 'polished';
 import styled, { css, DefaultTheme } from 'styled-components';
 import { ButtonProps } from '.';
 
 type WrapperProps = { hasIcon: boolean } & Pick<
   ButtonProps,
-  'size' | 'fullWidth'
+  'size' | 'fullWidth' | 'minimal'
 >;
 
 const wrapperModifiers = {
@@ -33,10 +34,18 @@ const wrapperModifiers = {
       }
     }
   `,
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+    }
+  `,
 };
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -49,7 +58,9 @@ export const Wrapper = styled.button<WrapperProps>`
     text-decoration: none;
 
     &:hover {
-      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+      background: ${minimal
+        ? 'none'
+        : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
 
     ${!!size && wrapperModifiers[size](theme)};
@@ -57,5 +68,7 @@ export const Wrapper = styled.button<WrapperProps>`
     ${!!fullWidth && wrapperModifiers.fullWidth()};
 
     ${!!hasIcon && wrapperModifiers.withIcon(theme)};
+
+    ${!!minimal && wrapperModifiers.minimal(theme)};
   `}
 `;
