4b936ae8344539c350382be366ccace1c1111f23
allexis096 <allexissoares@gmail.com>
2021-06-29 20:58:51 -0300

    :dizzy: chore: update Auth template

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -10,22 +10,28 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo id="banner" />
 
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
+        <div>
+          <Heading size="huge">All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
 
-      <S.Footer>Won Games 2021 © Todos os Direitos Reservados</S.Footer>
+        <S.Footer>Won Games 2021 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineleft>
-        {title}
-      </Heading>
+      <S.ContentWrapper>
+        <Logo id="content" color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineleft>
+          {title}
+        </Heading>
 
-      {children}
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 );
