8325859ab49ea5c1d74c6c30ad5f84ee55e050e7
allexis096 <allexissoares@gmail.com>
2021-07-04 18:10:50 -0300

    :dizzy: chore: update FormSignIn component

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,9 +1,41 @@
+import Link from 'next/link';
+import { Email, Lock } from '@styled-icons/material-outlined';
+
+import Button from 'components/Button';
+import TextField from 'components/TextField';
+import { FormLink, FormWrapper } from 'components/Form';
+
 import * as S from './styles';
 
 const FormSignIn = () => (
-  <S.Wrapper>
-    <h1>FormSignIn</h1>
-  </S.Wrapper>
+  <FormWrapper>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <S.ForgotPassword href="#">Forgot your password</S.ForgotPassword>
+
+      <Button size="large" fullWidth>
+        Sign in now
+      </Button>
+
+      <FormLink>
+        Do not have an account?{' '}
+        <Link href="/sign-up">
+          <a>Sign up</a>
+        </Link>
+      </FormLink>
+    </form>
+  </FormWrapper>
 );
 
 export default FormSignIn;
--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,15 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import FormSignIn from '.';
 
 describe('<FormSignIn />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<FormSignIn />);
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /sign in now/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />);
 
     expect(
-      screen.getByRole('heading', { name: /FormSignIn/i }),
+      screen.getByRole('link', { name: /forgot your password/i }),
     ).toBeInTheDocument();
+  });
 
-    expect(container.firstChild).toMatchSnapshot();
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument();
+    expect(screen.getByText(/do not have an account\?/i)).toBeInTheDocument();
   });
 });
