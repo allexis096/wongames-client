c26aa21332a37b3070aa26741b300d503000581a
allexis <allexissoares@gmail.com>
2021-09-19 20:29:33 -0300

    :zap: feat: update GameDetails componen to receive dynamic values

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,12 +1,76 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
-import GameDetails from '.';
+import GameDetails, { GameDetailsProps } from '.';
+
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'mac'],
+  releaseDate: '2021-11-21T23:00:00',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Arcade'],
+};
 
 describe('<GameDetails />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameDetails platforms={['mac']} />);
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />);
 
-    expect(screen.getByText(/Game Details/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Developer/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Release Date/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Platforms/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Publisher/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Rating/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: /Genres/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render platform icons', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument();
+    expect(
+      screen.queryByRole('img', { name: /linux/i }),
+    ).not.toBeInTheDocument();
+  });
+
+  it('should render the formated date', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText('Nov 21, 2021')).toBeInTheDocument();
+  });
+
+  it('should render free rating when BR0', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText(/free/i)).toBeInTheDocument();
+  });
+
+  it('should render 18+ rating when BR18', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR18" />);
+
+    expect(screen.getByText(/18\+/i)).toBeInTheDocument();
+  });
+
+  it('should render a list of genres', () => {
+    renderWithTheme(<GameDetails {...props} />);
+
+    expect(screen.getByText('Role-playing / Arcade')).toBeInTheDocument();
   });
 });
